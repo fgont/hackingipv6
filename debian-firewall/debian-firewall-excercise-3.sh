f49a47ffb91d597b63675016353089eee2dfff30
root <root@debian-firewall>
2016-06-23 21:02:32 -0400

    IMproved version

--- debian-firewall/debian-firewall-excercise-3.sh
+++ debian-firewall/debian-firewall-excercise-3.sh
@@ -28,14 +28,22 @@ ip6tables -A OUTPUT -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT
 
 # Allow outgoing ping
 ip6tables -A OUTPUT -p icmpv6 --icmpv6-type echo-request -j ACCEPT
+ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request -i eth0 -j ACCEPT
 ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-reply -j ACCEPT
 
 # Allow outgoing traceroute
 ip6tables -A INPUT -p icmpv6 --icmpv6-type port-unreachable -j ACCEPT
 ip6tables -A INPUT -p icmpv6 --icmpv6-type ttl-zero-during-transit -j ACCEPT
 
+
+# Allow outgoing traceroute FROM LOCAL NETWORK
+ip6tables -A OUTPUT -p icmpv6 --icmpv6-type port-unreachable -o eth0 -j ACCEPT
+ip6tables -A OUTPUT -p icmpv6 --icmpv6-type ttl-zero-during-transit -o eth0 -j ACCEPT
+
+
 # Block all other ICMPv6
-ip6tables -A FORWARD -p icmpv6 -j DROP
+ip6tables -A INPUT -p icmpv6 -j DROP
+ip6tables -A OUTPUT -p icmpv6 -j DROP
 
 
 # FORWARD traffic
@@ -43,8 +51,8 @@ ip6tables -A FORWARD -m ipv6header --header auth --soft -j DROP
 ip6tables -A FORWARD -m ipv6header --header esp --soft -j DROP
 ip6tables -A FORWARD -m ipv6header --header  dst --soft -j DROP
 ip6tables -A FORWARD -m ipv6header --header  route --soft -j DROP
-ip6tables -A FORWARD -m ipv6header --header  frag --soft -j DROP
-
+ip6tables -A FORWARD -m ipv6header --header  hop --soft -j DROP
+ip6tables -A FORWARD -m ipv6header --header  frag --soft -j ACCEPT
 ip6tables -A FORWARD -p tcp -j ACCEPT
 ip6tables -A FORWARD -p udp -j ACCEPT
 
